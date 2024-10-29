32f307372431c2ab3223bd95bcb988866b08e4ed
faofficial <astromfelicia98@gmail.com>
2024-10-29 11:32:22 +0100

    kod för exitbutton

---
+++ src/OtherButtons.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class OtherButtons
+{
+    public void exitButton(){
+        System.exit(0);
+    }
+}
