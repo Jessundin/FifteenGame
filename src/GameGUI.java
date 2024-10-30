0bf20322b3326f200ef42d64cc4470e11bd5d1b6
Jessica Sundin <jessundin@hotmail.com>
2024-10-30 19:16:52 +0100

    ändrat storleken på JButton arrayen och lagt till en if-sats för den 16-knappen som ska vara tom

--- src/GameGUI.java
+++ src/GameGUI.java
@@ -3,12 +3,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GameGUI extends JFrame implements ActionListener
-{
+public class GameGUI extends JFrame implements ActionListener {
 
     JPanel panel = new JPanel(new BorderLayout());
     JPanel centerPanel = new JPanel(new GridLayout(4, 4));
-    JButton[] buttons = new JButton[15];
+    JButton[] buttons = new JButton[16];
     JPanel eastPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
     JPanel westPanel = new JPanel(new FlowLayout());
     JButton newGameButton = new JButton("New Game");
@@ -16,12 +15,10 @@ public class GameGUI extends JFrame implements ActionListener
     JButton cheatButton = new JButton("Cheat");
     OtherButtons otherButtons = new OtherButtons();
 
-    public GameGUI()
-    {
+    public GameGUI() {
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("15 Puzzle Game");
-//        this.setResizable(false);
         this.setLocationRelativeTo(null);
 
         add(panel);
@@ -33,13 +30,16 @@ public class GameGUI extends JFrame implements ActionListener
         eastPanel.add(newGameButton);
         eastPanel.add(exitButton);
 
-        for (int i = 0; i < buttons.length; i++)
-        {
-            buttons[i] = new JButton(String.valueOf(i + 1));
-            buttons[i].setFocusable(Boolean.FALSE);
-            centerPanel.add(buttons[i], BorderLayout.CENTER);
+        for (int i = 0; i < buttons.length; i++) {
+            if (i == 15) {
+                buttons[i] = new JButton("");
+            } else {
+                buttons[i] = new JButton(String.valueOf(i + 1));
+            }
+                buttons[i].setFocusable(Boolean.FALSE);
+                centerPanel.add(buttons[i], BorderLayout.CENTER);
+                buttons[i].addActionListener(this);
         }
-        centerPanel.add(new JButton());
 
         newGameButton.addActionListener(this);
         exitButton.addActionListener(this);
@@ -50,22 +50,20 @@ public class GameGUI extends JFrame implements ActionListener
     }
 
     @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        if(e.getSource() == exitButton){
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == exitButton) {
             otherButtons.exitButton();
         }
-        if(e.getSource() == cheatButton){
+        if (e.getSource() == cheatButton) {
             centerPanel.removeAll();
-            otherButtons.solve(buttons,centerPanel);
+            otherButtons.solve(buttons, centerPanel);
             revalidate();
             repaint();
         }
-        if(e.getSource() == newGameButton){
+        if (e.getSource() == newGameButton) {
             centerPanel.removeAll();
             otherButtons.newGame(buttons);
-            for (JButton button : otherButtons.newGame(buttons))
-            {
+            for (JButton button : otherButtons.newGame(buttons)) {
                 centerPanel.add(button);
             }
             revalidate();
--- src/OtherButtons.java
+++ src/OtherButtons.java
@@ -4,15 +4,21 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class OtherButtons
-{
+public class OtherButtons {
+
+    private GameGUI gameGUI;
+
     public void exitButton(){
         System.exit(0);
     }
     public void solve(JButton[] buttons, JPanel centerPanel)
     {
         for (int i = 0; i < buttons.length; i++) {
-            buttons[i] = new JButton(String.valueOf(i + 1));
+            if (i == 15) {
+                buttons[i] = new JButton("");
+            } else {
+                buttons[i] = new JButton(String.valueOf(i + 1));
+            }
             buttons[i].setFocusable(Boolean.FALSE);
             centerPanel.add(buttons[i], BorderLayout.CENTER);
         }
