523338cc0e19fe05606416a1703bd9363de2d7f1
Jessica Sundin <jessundin@hotmail.com>
2024-11-03 22:49:35 +0100

    Final push new layout

--- src/PuzzleGameGUI.java
+++ src/PuzzleGameGUI.java
@@ -6,10 +6,9 @@ import java.awt.event.ActionListener;
 public class PuzzleGameGUI extends JFrame implements ActionListener {
 
     JPanel mainPanel = new JPanel(new BorderLayout());
-    JPanel gridPanel = new JPanel(new GridLayout(4, 4));
+    JPanel gridPanel = new JPanel(new GridLayout(4, 4, 5, 5));
     JButton[] puzzleButtons = new JButton[16];
-    JPanel eastPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-    JPanel westPanel = new JPanel(new FlowLayout());
+    JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
     JButton startNewGameButton = new JButton("New Game");
     JButton exitGameButton = new JButton("Exit");
@@ -29,17 +28,23 @@ public class PuzzleGameGUI extends JFrame implements ActionListener {
         setLocationRelativeTo(null);
 
         add(mainPanel);
+        gridPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
         mainPanel.add(gridPanel, BorderLayout.CENTER);
-        mainPanel.add(eastPanel, BorderLayout.EAST);
-        mainPanel.add(westPanel, BorderLayout.WEST);
+        southPanel.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
+        mainPanel.add(southPanel, BorderLayout.SOUTH);
 
-        westPanel.add(solvePuzzleButton);
-        eastPanel.add(startNewGameButton);
-        eastPanel.add(exitGameButton);
+        southPanel.setLayout(new GridLayout(3, 1, 5, 5));
+        southPanel.add(startNewGameButton).setFocusable(false);
+        southPanel.add(exitGameButton).setFocusable(false);
+        southPanel.add(solvePuzzleButton).setFocusable(false);
 
         for (int i = 0; i < puzzleButtons.length; i++) {
             puzzleButtons[i] = new JButton(i == 15 ? "" : String.valueOf(i + 1));
             puzzleButtons[i].setFocusable(false);
+            puzzleButtons[i].setPreferredSize(new Dimension(80, 80));
+            puzzleButtons[i].setFont(new Font("Arial", Font.PLAIN, 22));
+            puzzleButtons[i].setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+            puzzleButtons[i].setBorder(BorderFactory.createLineBorder(Color.BLACK));
             puzzleButtons[i].addActionListener(this);
             gridPanel.add(puzzleButtons[i]);
         }
